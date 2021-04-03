11c9ec7246be33991550a8f38af494f39398a1eb
Teemu Kurki <teemukurki4@gmail.com>
2021-04-03 20:44:15 +0300

    feat: added hot reload to devServer

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,11 +1,18 @@
 const merge = require("webpack-merge");
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 const common = require("./webpack.common");
 
 const config = {
   mode: "development",
   devtool: "source-map",
-  plugins: [new ForkTsCheckerWebpackPlugin()]
+  devServer: {
+    hot: true
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin(),
+    new ReactRefreshWebpackPlugin()
+  ]
 };
 
 module.exports = merge(common, config);
